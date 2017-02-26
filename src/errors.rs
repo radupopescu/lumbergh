ecb5af734e07eba000320d7be87fab022bac3b31
Radu Popescu <mail@radupopescu.net>
2017-02-26 23:32:17 +0100

    Use the nix crate instead of libc
    The "nix" crate is used for:
    
    * Signal operations
    * Process waiting
    * Process forking

--- src/errors.rs
+++ src/errors.rs
@@ -5,17 +5,11 @@ error_chain! {
 
     links {}
 
-    foreign_links {}
+    foreign_links {
+        NixError(nix::Error) #[doc="nix error"];
+    }
 
     errors {
-        ForkError(error_num: nix::Errno) {
-            description("Couldn't fork child process.")
-            display("Couldn't fork child process. Errno: {}", error_num)
-        }
-        SignalAPIError(function_name: String, error_num: i32) {
-            description("Signal API function error.")
-            display("Signal API function error: {} returned {}", function_name, error_num)
-        }
         SupervisorInitError {
             description("Could not initialize supervisor")
             display("Could not initialize supervisor")
--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,14 @@
 
 #[macro_use]
 extern crate error_chain;
-extern crate libc;
 extern crate nix;
 
-use libc::{waitpid, sigwait, pthread_sigmask, sigemptyset, sigfillset, sigaddset, sigset_t};
 #[cfg(not(target_os="linux"))]
-use libc::{c_int, sigaction};
-use libc::{SIG_BLOCK, SIGCHLD};
+use nix::c_int;
+#[cfg(not(target_os="linux"))]
+use nix::sys::signal::{SaFlags,SigHandler,SigAction,sigaction};
+use nix::sys::signal::{SigSet,Signal};
+use nix::sys::wait::waitpid;
 use nix::unistd::{fork,ForkResult};
 
 use errors::*;
@@ -21,29 +22,23 @@ pub fn run_supervisor<F>(child_fun: F) -> Result<()>
 {
     init_supervisor().chain_err(|| ErrorKind::SupervisorInitError)?;
 
-    match fork() {
-        Err(nix::Error::Sys(errno)) => Err(ErrorKind::ForkError(errno).into()),
-        Ok(ForkResult::Child) => child_fun(),
-        Ok(ForkResult::Parent { child }) => supervise(child),
-        _ => bail!("This should not happen!"),
+    match fork().chain_err(|| "Could not fork process.")? {
+        ForkResult::Child => child_fun(),
+        ForkResult::Parent { child } => supervise(child),
     }
 }
 
 fn init_supervisor() -> Result<()> {
     #[cfg(not(target_os="linux"))]
-    unsafe {
-        let mut act: sigaction = std::mem::uninitialized();
-        if sigemptyset(&mut act.sa_mask) == -1 {
-            return Err(ErrorKind::SignalAPIError("sigemptyset".to_owned(), -1).into());
-        }
-        act.sa_sigaction = std::mem::transmute(&null_handler);
-        act.sa_flags = 0;
-        if sigaction(SIGCHLD, &act, 0 as *mut sigaction) == -1 {
-            return Err(ErrorKind::SignalAPIError("sigaction".to_owned(), -1).into());
-        }
+    {
+        let mask = SigSet::empty();
+        let flags = SaFlags::empty();
+        let handler = SigHandler::Handler(null_handler);
+        let action = SigAction::new(handler, flags, mask);
+        unsafe { sigaction(Signal::SIGCHLD, &action)?; }
     }
 
-    mask_all_signals().chain_err(|| "Could not mask signals")?;
+    mask_all_signals()?;
 
     Ok(())
 }
@@ -51,51 +46,23 @@ fn init_supervisor() -> Result<()> {
 fn supervise(child_pid: i32) -> Result<()> {
     println!("Hey, {}. What's happening?", child_pid);
 
-    let mut signop: i32 = 0;
-    let wait_ret = unsafe {
-        let mut sigchld_set: sigset_t = std::mem::uninitialized();
-        if sigemptyset(&mut sigchld_set) == -1 {
-            return Err(ErrorKind::SignalAPIError("sigemptyset".to_owned(), -1).into());
-        }
-        if sigaddset(&mut sigchld_set, SIGCHLD) == -1 {
-            return Err(ErrorKind::SignalAPIError("sigaddset".to_owned(), -1).into());
-        }
-        sigwait(&sigchld_set, &mut signop)
-    };
-
-    if wait_ret != 0 {
-        return Err(ErrorKind::SignalAPIError("sigwait".to_owned(), wait_ret).into());
-    }
-
-    println!("Waitret: {}, Signop: {}", wait_ret, signop);
+    let mut sigchld = SigSet::empty();
+    sigchld.add(Signal::SIGCHLD);
+    sigchld.wait()?;
 
     // Wait for a child to finish.
-    let mut stat_val: i32 = 0;
-    let ret = unsafe { waitpid(child_pid, &mut stat_val, 0) };
-    println!("{} returned.", ret);
-    util::print_exit_status(stat_val);
+    let status = waitpid(child_pid, None)?;
+    println!("Status: {:?}", status);
 
     Ok(())
 }
 
 fn mask_all_signals() -> Result<()> {
-    unsafe {
-        let mut signal_mask: sigset_t = std::mem::uninitialized();
-        if sigfillset(&mut signal_mask) == -1 {
-            return Err(ErrorKind::SignalAPIError("sigfillset".to_owned(), -1).into());
-        }
-        let ret = pthread_sigmask(SIG_BLOCK, &signal_mask, 0 as *mut sigset_t);
-        if ret != 0 {
-            return Err(ErrorKind::SignalAPIError("pthread_sigmask".to_owned(), ret).into());
-        }
-        Ok(())
-    }
+    Ok(SigSet::all().thread_set_mask()?)
 }
 
 #[cfg(not(target_os="linux"))]
 extern "C" fn null_handler(_: c_int) -> () {}
-
-mod util;
 
 #[cfg(test)]
 mod tests {
